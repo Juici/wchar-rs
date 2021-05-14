98c9e4d8e87f85f589dd9fe68b1be8071c1e68f3
James <juicy66173@gmail.com>
2021-05-14 13:02:05 +0100

    Move target platform wchar_t logic to proc macro (#7)

--- impl/src/lib.rs
+++ impl/src/lib.rs
@@ -6,8 +6,10 @@ use std::fs;
 use std::path::PathBuf;
 
 use proc_macro2::{Span, TokenStream};
+use quote::ToTokens;
 use syn::{Error, Result};
 
+use crate::encode::Encode;
 use crate::parse::{IncludeInput, LitStrOrChar, WchInput, WchzInput};
 
 mod encode;
@@ -22,12 +24,24 @@ fn expand_macro<F: FnOnce() -> Result<TokenStream>>(f: F) -> proc_macro::TokenSt
 }
 
 #[proc_macro]
+pub fn wchar_t(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let _: syn::parse::Nothing = syn::parse_macro_input!(input);
+
+    let ty = libc::wchar_t::wchar_type();
+    ty.to_token_stream().into()
+}
+
+#[proc_macro]
 pub fn wch(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let WchInput { ty, literal, .. } = syn::parse_macro_input!(input);
 
-    expand_macro(|| match literal {
-        LitStrOrChar::Str(lit) => Ok(encode::expand_str(ty, &lit.value())),
-        LitStrOrChar::Char(lit) => encode::expand_char(ty, lit),
+    expand_macro(|| {
+        let ty = ty.map(|(ty, _)| ty);
+
+        match literal {
+            LitStrOrChar::Str(lit) => Ok(encode::expand_str(ty, &lit.value())),
+            LitStrOrChar::Char(lit) => encode::expand_char(ty, lit),
+        }
     })
 }
 
@@ -36,7 +50,9 @@ pub fn wchz(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let WchzInput { ty, literal, .. } = syn::parse_macro_input!(input);
 
     expand_macro(|| {
+        let ty = ty.map(|(ty, _)| ty);
         let text = literal.value();
+
         if text.as_bytes().contains(&0) {
             return Err(Error::new(
                 literal.span(),
@@ -53,6 +69,7 @@ pub fn include_wch(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let IncludeInput { ty, file_path, .. } = syn::parse_macro_input!(input);
 
     expand_macro(|| {
+        let ty = ty.map(|(ty, _)| ty);
         let text = read_file(&file_path)?;
 
         Ok(encode::expand_str(ty, &text))
@@ -64,6 +81,7 @@ pub fn include_wchz(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let IncludeInput { ty, file_path, .. } = syn::parse_macro_input!(input);
 
     expand_macro(|| {
+        let ty = ty.map(|(ty, _)| ty);
         let text = read_file(&file_path)?;
 
         if text.as_bytes().contains(&0) {
--- impl/src/parse.rs
+++ impl/src/parse.rs
@@ -1,6 +1,7 @@
-use proc_macro2::Ident;
-use syn::parse::{Parse, ParseStream, Result};
-use syn::{Error, LitChar, LitStr, Token};
+use proc_macro2::TokenStream;
+use quote::ToTokens;
+use syn::parse::{Lookahead1, Parse, ParseStream, Result};
+use syn::{LitChar, LitStr, Token};
 
 mod kw {
     syn::custom_keyword!(u16);
@@ -16,6 +17,15 @@ pub enum WCharType {
     I32(kw::i32),
 }
 
+impl WCharType {
+    fn peek(lookahead: &Lookahead1) -> bool {
+        lookahead.peek(kw::u16)
+            || lookahead.peek(kw::u32)
+            || lookahead.peek(kw::i16)
+            || lookahead.peek(kw::i32)
+    }
+}
+
 impl Parse for WCharType {
     fn parse(input: ParseStream) -> Result<Self> {
         let lookahead = input.lookahead1();
@@ -28,12 +38,18 @@ impl Parse for WCharType {
         } else if lookahead.peek(kw::i32) {
             Ok(WCharType::I32(input.parse()?))
         } else {
-            // HACK: Provide nicer compiler errors.
-            let mut err = lookahead.error();
-            if let Ok(ident) = input.parse::<Ident>() {
-                err = Error::new(ident.span(), err);
-            }
-            Err(err)
+            Err(lookahead.error())
+        }
+    }
+}
+
+impl ToTokens for WCharType {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        match self {
+            WCharType::U16(ty) => ty.to_tokens(tokens),
+            WCharType::U32(ty) => ty.to_tokens(tokens),
+            WCharType::I16(ty) => ty.to_tokens(tokens),
+            WCharType::I32(ty) => ty.to_tokens(tokens),
         }
     }
 }
@@ -57,48 +73,69 @@ impl Parse for LitStrOrChar {
 }
 
 pub struct WchInput {
-    pub ty: WCharType,
-    pub comma: Token![,],
+    pub ty: Option<(WCharType, Token![,])>,
     pub literal: LitStrOrChar,
 }
 
 impl Parse for WchInput {
     fn parse(input: ParseStream) -> Result<Self> {
+        let lookahead = input.lookahead1();
+        let ty = if WCharType::peek(&lookahead) {
+            Some((input.parse()?, input.parse()?))
+        } else if lookahead.peek(LitStr) || lookahead.peek(LitChar) {
+            None
+        } else {
+            return Err(lookahead.error());
+        };
+
         Ok(WchInput {
-            ty: input.parse()?,
-            comma: input.parse()?,
+            ty,
             literal: input.parse()?,
         })
     }
 }
 
 pub struct WchzInput {
-    pub ty: WCharType,
-    pub comma: Token![,],
+    pub ty: Option<(WCharType, Token![,])>,
     pub literal: LitStr,
 }
 
 impl Parse for WchzInput {
     fn parse(input: ParseStream) -> Result<Self> {
+        let lookahead = input.lookahead1();
+        let ty = if WCharType::peek(&lookahead) {
+            Some((input.parse()?, input.parse()?))
+        } else if lookahead.peek(LitStr) {
+            None
+        } else {
+            return Err(lookahead.error());
+        };
+
         Ok(WchzInput {
-            ty: input.parse()?,
-            comma: input.parse()?,
+            ty,
             literal: input.parse()?,
         })
     }
 }
 
 pub struct IncludeInput {
-    pub ty: WCharType,
-    pub comma: Token![,],
+    pub ty: Option<(WCharType, Token![,])>,
     pub file_path: LitStr,
 }
 
 impl Parse for IncludeInput {
     fn parse(input: ParseStream) -> Result<Self> {
+        let lookahead = input.lookahead1();
+        let ty = if WCharType::peek(&lookahead) {
+            Some((input.parse()?, input.parse()?))
+        } else if lookahead.peek(LitStr) {
+            None
+        } else {
+            return Err(lookahead.error());
+        };
+
         Ok(IncludeInput {
-            ty: input.parse()?,
-            comma: input.parse()?,
+            ty,
             file_path: input.parse()?,
         })
     }
